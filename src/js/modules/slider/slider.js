d92e82ea12510cf8400d7af5e00c244155de42f7
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-27 18:49:25 +0400

    change class Slider

--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -1,5 +1,13 @@
 export class Slider {
-   constructor({ page = '', btns = '', next = '', prev = '' } = {}) {
+   constructor({
+      page = null,
+      btns = null,
+      next = null,
+      prev = null,
+      activeClass,
+      animate,
+      autoplay,
+   } = {}) {
       //    внутри конструктора указываем переменные
       this.page = document.querySelector(page);
       //   слайдеры это у нас все дети родительского блока с классом page
@@ -7,5 +15,10 @@ export class Slider {
       this.btns = document.querySelectorAll(btns);
       //   номер текущего слайда
       this.slideIndex = 1;
+      this.prev = document.querySelector(prev);
+      this.next = document.querySelector(next);
+      this.activeClass = activeClass;
+      this.animate = animate;
+      this.autoplay = autoplay;
    }
 }
